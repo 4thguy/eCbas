a16550cabcb6331b5b2a54d45e05b17c61123c55
Ivan Ellul Xuereb <ivanxuereb@gmail.com>
2018-07-18 15:26:51 +0200

    add working components for front page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { FrontPageComponent } from './front-page/front-page.component';
 import { ItemDetailsComponent } from './item-details/item-details.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: 'items', component: ItemDetailsComponent },
   { path: 'item-details/:id', component: ItemDetailsComponent },
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent },
-  { path: '', component: AppComponent },
+  { path: '', component: FrontPageComponent },
   { path: '*', redirectTo: '' },
 ];
 
