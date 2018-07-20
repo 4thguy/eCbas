ae97629d60242f9062c3fd22bc6b45e7e3d1974c
Ivan Ellul Xuereb <ivanxuereb@gmail.com>
2018-07-20 11:27:46 +0200

    item details bulletproofing

--- src/app/item-details/item-details.component.ts
+++ src/app/item-details/item-details.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import * as userActions from '../actions/user';
+import * as userReducer from '../reducers/user';
+import { User } from '../user';
 
 import { ActivatedRoute } from '@angular/router';
 
@@ -12,15 +19,20 @@ import { Item } from '../item';
   styleUrls: ['./item-details.component.css']
 })
 export class ItemDetailsComponent implements OnInit {
+  user$: Observable<any>;
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private itemService: ItemService,
+    private store : Store<userReducer.State>,
   ) {
+    this.user$ = this.store.select('user');
   }
 
   items: Item[];
   item: Item;
+  itemFound: boolean = true;
 
   ngOnInit() {
     this.getItems();
@@ -32,6 +44,11 @@ export class ItemDetailsComponent implements OnInit {
         this.getItem();
       }
     );
+    this.user$.subscribe((userState) => {
+      if (userState.user === null) {
+        this.goOut();
+      }
+    });
   }
 
   getItems(): void {
@@ -43,8 +60,24 @@ export class ItemDetailsComponent implements OnInit {
     var id = + this.route.snapshot.paramMap.get('id');
     if (id > 0) {
       this.itemService.getItem(id)
-        .subscribe(item => this.item = item);
+        .subscribe(item => {
+          //basic validation to see if we can display the item
+          if (item === undefined) {
+            this.itemFound = false;
+            this.item = undefined;
+          } else {
+            this.itemFound = true;
+            this.item = item
+          }
+        });
     }
+  }
+
+  //ToDo:
+  //this is a very simple way of protecting the page from unauthorized users
+  //ideally we make this check before even loading the page to protect the data
+  goOut() {
+    this.router.navigate(['/']);
   }
 
 }
